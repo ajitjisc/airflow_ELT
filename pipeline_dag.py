698040f604e902c13f35f595c8ffe72952791438
Ajit Chandran <ajit.chandran@jisc.ac.uk>
2023-03-13 15:55:57 +0000

    add new files

--- pipeline_dag.py
+++ pipeline_dag.py
@@ -3,9 +3,10 @@ from airflow import DAG
 from airflow.operators.python_operator import PythonOperator
 from airflow.utils.dates import days_ago
 from datetime import datetime
-from pipeline_etl import run_data_pipeline
-
-
+from get_institutions import generate_institutions
+from get_datax_enabled_institutions import get_datax_enabled_institutions
+from check_report_to_run import check_attendance_plus_and_generate_reports
+from check_reports_update import check_report1,check_report2,check_report3
 default_args = {
     'owner': 'airflow',
     'depends_on_past': False,
@@ -18,16 +19,53 @@ default_args = {
 }
 
 dag = DAG(
-    'strava_dag',
+    'datadrop_test__dag',
     default_args=default_args,
-    description='Ajit strava ETL process!',
+    description='test DataDrop ETL process!',
     schedule_interval=timedelta(days=1),
 )
 
-run_etl = PythonOperator(
-    task_id='complete_strava_etl',
-    python_callable=run_data_pipeline,
+#task_1
+task_1 = PythonOperator(
+    task_id='get the list of institutions and laconfig details',
+    python_callable=generate_institutions,
+    dag=dag, 
+)
+#task_2
+task_2 = PythonOperator(
+    task_id='get datax enabled institutions',
+    python_callable=get_datax_enabled_institutions,
+    dag=dag, 
+)
+#task_3
+task_3 = PythonOperator(
+    task_id='get the report flags and generate reports',
+    python_callable=check_attendance_plus_and_generate_reports,
     dag=dag, 
 )
 
-run_etl
+#task_4
+task_4 = PythonOperator(
+    task_id='lastactivity_report',
+    python_callable=check_report1,
+    dag=dag, 
+)
+task_4.set_upstream(task_3)
+
+#task_5
+task_5 = PythonOperator(
+    task_id='tlc_report',
+    python_callable=check_report2,
+    dag=dag, 
+)
+task_5.set_upstream(task_3)
+#task_6
+task_6 = PythonOperator(
+    task_id='attendance_report',
+    python_callable=check_report3,
+    dag=dag, 
+)
+task_6.set_upstream(task_3)
+
+task_1 >> task_2 >> task_3
+
