12174320e5f5127ac9ac9c5e6acf1c47e9e93407
Ajit Chandran <ajit.chandran@jisc.ac.uk>
2023-03-22 21:01:27 +0000

    update

--- check_report_to_run.py
+++ check_report_to_run.py
@@ -3,11 +3,11 @@ from get_datax_enabled_institutions import get_datax_enabled_institutions
 from reports import generate_report1,generate_report2,generate_report3,generate_report3_fail
 
 import os
-
+# get dag directory path
 def check_attendance_plus_and_generate_reports():
     institutions = get_datax_enabled_institutions()
     for institution in institutions:
-        reports_dir = os.path.join('la-data',institution['name'], 'reports')
+        reports_dir = os.path.join('/home/ubuntu/la-data',institution['name'], 'reports')
         os.makedirs(reports_dir, exist_ok=True)
         if institution['function-attendance-plus-enabled']:
             generate_report3(os.path.join(reports_dir, 'attendance.tsv'))
